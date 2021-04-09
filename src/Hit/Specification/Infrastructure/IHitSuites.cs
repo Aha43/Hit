6fd05a344efc63e25b2c32988e43754472924390
Arne Halvorsen <arne.halvorsen@gmail.com>
2021-04-09 16:49:03 +0200

    #20 completed

--- src/Hit/Specification/Infrastructure/IHitSuites.cs
+++ src/Hit/Specification/Infrastructure/IHitSuites.cs
@@ -6,6 +6,7 @@ namespace Hit.Specification.Infrastructure
     public interface IHitSuites<World>
     {
         IEnumerable<IHitSuite<World>> Suites { get; }
+        IHitSuite<World> GetNamedSuite(string name);
         Task<IEnumerable<IHitSuiteTestResults>> RunTestsAsync();
     }
 }
