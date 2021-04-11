0cb106697bb76d299fb5c3d48c77597915bfb2ac
Arne Halvorsen <arne.halvorsen@gmail.com>
2021-04-11 11:06:59 +0200

    #30 and #31 done

---
+++ src/Hit/Infrastructure/Exceptions/SuiteNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Hit.Infrastructure.Exceptions
+{
+    public class SuiteNotFoundException : Exception
+    {
+        public SuiteNotFoundException(string name) : base(name)
+        {
+        }
+    }
+}
--- src/Hit/Infrastructure/HitSuites.cs
+++ src/Hit/Infrastructure/HitSuites.cs
@@ -1,4 +1,5 @@
-﻿using Hit.Specification.Infrastructure;
+﻿using Hit.Infrastructure.Exceptions;
+using Hit.Specification.Infrastructure;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,26 +9,41 @@ namespace Hit.Infrastructure
 {
     public class HitSuites<World> : IHitSuites<World>
     {
-        private readonly List<IHitSuite<World>> _suites = new List<IHitSuite<World>>();
+        private readonly Dictionary<string, IHitSuite<World>> _suites = new Dictionary<string, IHitSuite<World>>();
 
         public HitSuites<World> AddSuite(Action<HitSuiteOptions> conf = null)
         {
-            _suites.Add(new HitSuite<World>(conf));
+            var suite = new HitSuite<World>(conf);
+
+            if (string.IsNullOrWhiteSpace(suite.Name))
+            {
+                throw new ArgumentException("A suite in suites must have name");
+            }
+            if (_suites.ContainsKey(suite.Name))
+            {
+                throw new ArgumentException("Duplicate suite name: " + suite.Name);
+            }
+
+            _suites.Add(suite.Name, suite);
             return this;
         }
 
-        public IEnumerable<IHitSuite<World>> Suites => _suites.AsReadOnly();
-
+        public IEnumerable<IHitSuite<World>> Suites => _suites.Values;
+        
         public IHitSuite<World> GetNamedSuite(string name)
         {
-            if (string.IsNullOrWhiteSpace(name)) return null;
-            return _suites.Find(e => name.Equals(e.Name));
+            if (_suites.TryGetValue(name, out IHitSuite<World> retVal))
+            {
+                return retVal;
+            }
+
+            throw new SuiteNotFoundException(name);
         }
 
         public async Task<IEnumerable<IHitSuiteTestResults>> RunTestsAsync()
         {
-            var suites = _suites.ToArray();
-            var n = suites.Count();
+            var suites = Suites.ToArray();
+            var n = suites.Length;
             var retVal = new IHitSuiteTestResults[n];
             for (var i = 0; i < n; i++)
             {
