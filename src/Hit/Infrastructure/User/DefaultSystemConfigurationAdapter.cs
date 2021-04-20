42a2d9f7a922e2b35804dfeac27c818e90bb0299
Arne Halvorsen <arne.halvorsen@gmail.com>
2021-04-20 18:52:01 +0200

    #60 done

--- src/Hit/Infrastructure/User/DefaultSystemConfigurationAdapter.cs
+++ src/Hit/Infrastructure/User/DefaultSystemConfigurationAdapter.cs
@@ -1,22 +1,29 @@
 ﻿using Microsoft.Extensions.Configuration;
+using System.Reflection;
 
 namespace Hit.Infrastructure.User
 {
     public abstract class DefaultSystemConfigurationAdapter<World> : SystemConfigurationAdapter<World>
     {
-        private readonly string _jsonPath;
+        private string _jsonPath;
+        private Assembly _assembly;
 
-        public DefaultSystemConfigurationAdapter() => _jsonPath = null;
-        public DefaultSystemConfigurationAdapter(string jsonPath) => _jsonPath = jsonPath;
+        protected void UserSecretForClient(object client) => _assembly = client?.GetType().Assembly;
+
+        protected void LoadFromJsonFile(string path) => _jsonPath = path;
 
         public override IConfiguration GetConfiguration()
         {
-            var builder = new ConfigurationBuilder()
-                .AddUserSecrets<DefaultSystemConfigurationAdapter<World>>();
+            var builder = new ConfigurationBuilder();
                 
             if (!string.IsNullOrWhiteSpace(_jsonPath))
             {
                 builder.AddJsonFile(_jsonPath, true);
+            }
+
+            if (_assembly != null)
+            {
+                builder.AddUserSecrets(_assembly, true);
             }
 
             var retVal = builder.Build();
