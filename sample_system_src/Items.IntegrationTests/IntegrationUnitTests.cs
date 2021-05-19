d0f1827c0a2d0b05a5db6355aa44dbde41ad0029
Arne Halvorsen <arne.halvorsen@gmail.com>
2021-05-19 18:55:23 +0200

    Updated how integration test are written (a single xunit theory method) after devalop history dogfooding Hit in day time work project

---
+++ sample_system_src/Items.IntegrationTests/IntegrationUnitTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Items.IntegrationTests
+{
+    public class IntegrationUnitTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
